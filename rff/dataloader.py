b9d399693690bef9a8794e88c4289a472c7a74c0
Joshua Long <44989821+jmclong@users.noreply.github.com>
2021-05-09 22:54:24 -0400

    Updated doc

--- rff/dataloader.py
+++ rff/dataloader.py
@@ -2,18 +2,19 @@ import torch
 
 from torch import Tensor
 
+
 def rectangular_coordinates(size: tuple) -> Tensor:
-    r"""Creates a tensor
+    r"""Creates a tensor of equally spaced coordinates for use with something like and image or volume
 
     Args:
-        size (tuple): [description]
+        size (tuple): shape of the image or volume
 
     Returns:
-        Tensor: [description]
+        Tensor: tensor of shape :math:`(\text{minibatch}, \prod_i \text{size}_i)`
     """
-    linspace_func = lambda nx: torch.linspace(0, 1, nx)
+    def linspace_func(nx): return torch.linspace(0, 1, nx)
     linspaces = map(linspace_func, size)
     coordinates = torch.meshgrid(*linspaces)
-    flatten_func = lambda xc: torch.flatten(xc)
+    def flatten_func(xc): return torch.flatten(xc)
     flattened = map(flatten_func, coordinates)
     return torch.stack(tuple(flattened), axis=-1)
--- rff/layers.py
+++ rff/layers.py
@@ -6,23 +6,22 @@ import rff.functional as rffF
 
 
 class RandomFourierFeatures(nn.Module):
+    """Layer for mapping coordinates using random Fourier features"""
+
     def __init__(self, sigma: float, input_size: float,
-                 hidden_size: float, b: Optional[Tensor] = None):
+                 encoded_size: float, b: Optional[Tensor] = None):
         r"""Computes :math:`\gamma(\mathbf{v}) = (\cos{2 \pi \mathbf{B} \mathbf{v}} , \sin{2 \pi \mathbf{B} \mathbf{v}})`
 
         Args:
-            sigma (float): standard deviation for 
-            input_size (float): size of the 
-            hidden_size (float): [description]
+            sigma (float): standard deviation
+            input_size (float): the number of input dimensions
+            encoded_size (float): the number of dimensions the `b` matrix maps to
             b (Optional[Tensor], optional): Optionally specify a :attr:`b` matrix already sampled
         """
         super().__init__()
         if b is None:
-            self.b = nn.parameter.Parameter(
-                rffF.sample_b(sigma, (hidden_size, input_size)),
-                requires_grad=False)
-        else:
-            self.b = nn.parameter.Parameter(b, requires_grad=False)
+            b = rffF.sample_b(sigma, (encoded_size, input_size))
+        self.b = nn.parameter.Parameter(b, requires_grad=False)
 
     def forward(self, v: Tensor) -> Tensor:
         r"""Maps regular coordinates using random Fourier features
@@ -31,6 +30,6 @@ class RandomFourierFeatures(nn.Module):
             v (Tensor): Tensor of regular coordinates of size :math:`(\text{minibatch}, \text{input_size})`
 
         Returns:
-            Tensor: [description]
+            Tensor: Tensor mapping using random fourier features of shape :math:`(\text{minibatch}, \text{2 \cdot encoded_size})`
         """
         return rffF.random_fourier_features(v, self.b)
