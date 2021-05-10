b47f8475ae92ced0a10ce2a14ad63030b3467bfe
Joshua Long <44989821+jmclong@users.noreply.github.com>
2021-05-09 23:43:26 -0400

    pep8

--- test/test_layers.py
+++ test/test_layers.py
@@ -1,5 +1,4 @@
 import numpy as np
-import torch
 import unittest
 import rff
 
@@ -12,4 +11,7 @@ class LayerTest(unittest.TestCase):
         v = rff.dataloader.rectangular_coordinates((256, 256))
         gamma_v = layer(v)
         gamma_v_expected = rff.functional.random_fourier_features(v, b)
-        np.testing.assert_almost_equal(gamma_v.numpy(), gamma_v_expected.numpy(), decimal=5)
+        np.testing.assert_almost_equal(
+            gamma_v.numpy(),
+            gamma_v_expected.numpy(),
+            decimal=5)
