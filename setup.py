ef934f9791efc6ae09156a93f11c660bea55336d
Joshua Long <44989821+jmclong@users.noreply.github.com>
2021-05-10 21:33:09 -0400

    Fix the build

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name='random-fourier-features-pytorch',
       license='BSD-3',
       packages=['rff'],
       install_requires=["numpy>=1.17",
-                        "torch>=1.0"])
+                        "torch>=1.7",
+                        "torchvision>=0.9"])
