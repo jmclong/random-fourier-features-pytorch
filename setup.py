3fa6f76b876d1f6fbb085e067237f6434f251df5
Joshua Mark Coley Long <44989821+jmclong@users.noreply.github.com>
2022-05-17 21:53:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='random-fourier-features-pytorch',
       version='0.2',
       description='Random Fourier Features for PyTorch',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/jmclong/random-fourier-features-pytorch',
       author='Joshua Long',
       author_email='joshualong@live.com',
